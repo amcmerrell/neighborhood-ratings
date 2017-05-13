25bb5d7a3b97fd935c281d6d816180fc7ac9ad1a
Andrew Merrell <andrew.m.merrell@gmail.com>
2017-05-12 18:05:30 -0700

    remove pagefront deployment dependencies.

--- config/deploy.js
+++
@@ -1,8 +0,0 @@
-// module.exports = function(deployTarget) {  
-//   return {
-//     pagefront: {
-//       app: 'neighborly-pdx',
-//       key: process.env.PAGEFRONT_KEY
-//     }
-//   };
-// };
